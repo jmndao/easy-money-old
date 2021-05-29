7cb6585ece0d7d982b75ffdf98b9bab320cd27a0
Jonathan Moussa NDAO <jmndao@gmail.com>
2021-05-29 11:42:20 +0000

    Easy-money on django server

---
+++ emServ/dashboard/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+# First Model -- Stock Depot/Achat
+
+
+# Second Model -- Demande Client
+
+
+# Third Model -- Clients
+
+
+# Fourth Model -- Notifications
--- js/script.js
+++
@@ -1,7 +0,0 @@
-// Setting up the Modal
-const modal = document.querySelector('.modal');
-M.Modal.init(modal, {});
-
-// Setting up the Select
-const select = document.querySelectorAll('select');
-M.FormSelect.init(select, {});
