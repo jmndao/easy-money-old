f33bf8b6044d8f8d0656ec0ef0b1faccd6c6e073
jmndao <jmndao@gmail.com>
2021-06-07 00:57:33 +0000

    Config

--- emServ/dashboard/admin.py
+++ emServ/dashboard/admin.py
@@ -4,12 +4,12 @@ from django.contrib import admin
 from .models import ProductModel
 from .models import DepositStockModel
 from .models import BuyingStockModel
-from .models import Demande_client
+from .models import ClientRequestModel
 from .models import ClientModel
 
 admin.site.register(ProductModel)
 admin.site.register(DepositStockModel)
 admin.site.register(BuyingStockModel)
-admin.site.register(Demande_client)
+admin.site.register(ClientRequestModel)
 admin.site.register(ClientModel)
 
--- emServ/easy_money/settings.py
+++ emServ/easy_money/settings.py
@@ -80,10 +80,11 @@ WSGI_APPLICATION = 'easy_money.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER' : config('DB_USER'),
-        'PASSWORD' : config('DB_PASSWORD'),
-        'HOST' : config('DB_HOST')
+        'NAME': 'dbTest',
+        'USER' : 'jmndao',
+        'PASSWORD' : 'Passer123',
+        'PORT': 5432,
+        'HOST' : 'localhost'
     }
 }
 
