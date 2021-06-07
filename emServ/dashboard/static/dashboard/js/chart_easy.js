65f3625576aacb884afef25626065921c93e73f1
Abdoul Akhad Top <akhad0015@gmail.com>
2021-06-07 22:49:02 +0000

    separate achat et depot

--- emServ/dashboard/static/dashboard/js/chart_easy.js
+++ emServ/dashboard/static/dashboard/js/chart_easy.js
@@ -1,13 +1,13 @@
 var chart_achat = document.getElementById('myChart');
-chart_achat.height = 200;
+chart_achat.height = 300;
 var myChart_achat = new Chart(chart_achat, {
     type: 'bar',
     data: {
-        labels: ['Janvier', 'Fevrier', 'March', 'Avril', 'Mais', 'Juin','juillet','Aout','Septembre','Octobre','Novembre','Decembre'],
+        labels: ['Janv', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin','Juil','Aout','Sept','Oct','Nov','Dec'],
         datasets: [{
             label: 'Stock Achat',
             data: [120000, 212700, 100000, 212000, 312000, 97000, 212700,120000 ,97000, 170000, 212000,147000],
-            fill: false,
+            fill: true,
             tension: 0.2,
             borderColor: 
                 'rgba(255, 99, 132, 1)',
@@ -42,11 +42,11 @@ var myChart_achat = new Chart(chart_achat, {
 });
 
 var chart_depot = document.getElementById('myChart2');
-chart_depot.height = 200;
+chart_depot.height = 300;
 var chart_depot = new Chart(chart_depot, {
     type: 'bar',
     data: {
-        labels: ['Janvier', 'Fevrier', 'March', 'Avril', 'Mais', 'Juin','juillet','Aout','Septembre','Octobre','Novembre','Decembre'],
+        labels: ['Janv', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin','Juil','Aout','Sept','Oct','Nov','Dec'],
         datasets: [{
             label: 'Stock Achat',
             data: [ 212700,120000 ,97000, 170000, 212000,147000, 120000, 212700, 100000, 212000, 312000, 97000, ],
