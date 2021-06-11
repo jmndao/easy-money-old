f44d4b7afc9f33cf02dc835a939d30a57541b557
Jonathan Moussa NDAO <jmndao@gmail.com>
2021-06-11 07:30:57 +0000

    Fixing chart

--- emServ/dashboard/static/dashboard/js/chart_easy.js
+++ emServ/dashboard/static/dashboard/js/chart_easy.js
@@ -1,83 +1,31 @@
-var chart_achat = document.getElementById('myChart');
-chart_achat.height = 300;
-var myChart_achat = new Chart(chart_achat, {
-    type: 'bar',
-    data: {
-        labels: ['Janv', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin','Juil','Aout','Sept','Oct','Nov','Dec'],
-        datasets: [{
-            label: 'Stock Achat',
-            data: [120000, 212700, 100000, 212000, 312000, 97000, 212700,120000 ,97000, 170000, 212000,147000],
-            fill: true,
-            tension: 0.2,
-            borderColor: 
-                'rgba(255, 99, 132, 1)',
-            borderWidth: 1
-        }]
-    },
-    options: {
-        title:{
-            display:true,
-            text: 'Montant Total Recu Par Mois ',
-            fontSize:25
-        },
-        legend:{
-            position:'bottom',
-            display:false,
-        },
-        layout:{
-            // padding:{
-            //     left: 50,
-            //     right: 0
-            // }
-        },
-        scales: {
-            yAxes: [{
-                
-                ticks: {
-                    beginAtZero: true,
-                }
-            }]
-        }
-    }
-});
+var ctx = document.getElementById("myChart").getContext("2d");
 
-var chart_depot = document.getElementById('myChart2');
-chart_depot.height = 300;
-var chart_depot = new Chart(chart_depot, {
+var data = {
+    labels: ['Janv', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aout', 'Sept', 'Oct', 'Nov', 'Dec'],
+    datasets: [{
+            label: 'Depot',
+            data: [120000, 212700, 100000, 212000, 312000, 97000, 212700, 120000, 97000, 170000, 212000, 147000],
+            backgroundColor: 'rgb(255, 61, 0)'
+
+        },
+        {
+            label: 'Achat',
+            data: [130000, 214700, 40000, 262000, 412000, 87000, 200700, 143000, 67000, 320000, 232000, 184000],
+            backgroundColor: 'rgb(0, 0, 0)'
+
+        }
+    ]
+};
+
+var myBarChart = new Chart(ctx, {
     type: 'bar',
-    data: {
-        labels: ['Janv', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin','Juil','Aout','Sept','Oct','Nov','Dec'],
-        datasets: [{
-            label: 'Stock Achat',
-            data: [ 212700,120000 ,97000, 170000, 212000,147000, 120000, 212700, 100000, 212000, 312000, 97000, ],
-            fill: false,
-            tension: 0.2,
-            borderColor: 
-                'rgba(255, 99, 132, 1)',
-            borderWidth: 1
-        }]
-    },
+    data: data,
     options: {
-        title:{
-            display:true,
-            text: 'Montant Dépensé Par Mois',
-            fontSize:25
-        },
-        legend:{
-            position:'bottom',
-            display:false,
-        },
-        layout:{
-            // padding:{
-            //     left: 50,
-            //     right: 0
-            // }
-        },
+        barValueSpacing: 0,
         scales: {
             yAxes: [{
-                
                 ticks: {
-                    beginAtZero: true,
+                    min: 0,
                 }
             }]
         }
