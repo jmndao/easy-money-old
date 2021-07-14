8321a562dae9353b3e61114c9f8a00c3e6a065d6
Abdoul Akhad Top <akhad0015@gmail.com>
2021-07-14 00:01:23 +0000

    made 404 and 500 pages

--- emServ/easy_money/settings.py
+++ emServ/easy_money/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', cast=bool)
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1',
 #                  config('SERVER', default='127.0.0.1')]
