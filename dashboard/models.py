cc72189d1bca3ea8504e8183f638818378ebc5cc
Jonathan Moussa NDAO <jmndao@gmail.com>
2021-07-28 22:39:02 +0000

    Modifying name in dashboard.models mati(e)re -> material

--- dashboard/models.py
+++ dashboard/models.py
@@ -173,7 +173,7 @@ class ProductModel(models.Model):
     sold = models.BooleanField(default=False)
     color = models.CharField(max_length=100, blank=True, null=True)
     connectic = models.BooleanField(default=False)
-    matiere = models.CharField(
+    material = models.CharField(
         max_length=100, null=True, blank=True, default=None)
     garantie = models.BooleanField(default=False)
     delai_garantie = models.IntegerField(null=True, blank=True, default=True)
