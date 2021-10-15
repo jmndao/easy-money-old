ef2e570e27fbe1da8509d802d29762ba43bb377d
jmndao <jmndao@gmail.com>
2021-10-15 08:20:26 +0000

    Setting and Vente template
    - Email configure to custom domain
    
    - Liste des nouveaux clients instead ...

--- easy_money/settings.py
+++ easy_money/settings.py
@@ -171,8 +171,8 @@ DROPBOX_ROOT_PATH = config('DROPBOX_ROOT_PATH')
 ##########################
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
