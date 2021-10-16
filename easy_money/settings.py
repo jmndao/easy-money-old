86f9d658a28bdd9562af8d48b65794f4d1cb07f4
jmndao <jmndao@gmail.com>
2021-10-16 16:35:11 +0000

    livereloader middleware removed

--- easy_money/settings.py
+++ easy_money/settings.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # Adding whitenoise
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 
